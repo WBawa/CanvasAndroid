82a5302cc258e41cac55622e4c29f8076d99a476
Wahid Bawa <wahidbawa10@gmail.com>
2021-05-13 12:26:49 -0400

    pain

--- BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
+++ BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.riis.biogen
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var radioButton: RadioButton
+    private lateinit var createButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioGroup = findViewById(R.id.radiogroup)
+        createButton = findViewById(R.id.create_button)
+
+        createButton.setOnClickListener {
+
+        }
+
+    }
+
+    fun checkMajor(v: View) {
+        val radioId = radioGroup.checkedRadioButtonId
+        radioButton = findViewById(radioId)
+        Log.d(TAG, radioButton.text.toString())
+
     }
 }
