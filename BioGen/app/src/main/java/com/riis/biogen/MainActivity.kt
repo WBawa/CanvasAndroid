fd62984e90852c6e5d3e6833de0afa62ea89d9c4
Wahid Bawa <wahidbawa10@gmail.com>
2021-05-13 13:00:25 -0400

    populated the spinner

--- BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
+++ BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
@@ -3,15 +3,14 @@ package com.riis.biogen
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var radioGroup: RadioGroup
     private lateinit var radioButton: RadioButton
     private lateinit var createButton: Button
+    private lateinit var spinner: Spinner
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         radioGroup = findViewById(R.id.radiogroup)
         createButton = findViewById(R.id.create_button)
+        spinner = findViewById(R.id.spinner)
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.majors, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
         createButton.setOnClickListener {
 
@@ -28,9 +32,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkMajor(v: View) {
-        val radioId = radioGroup.checkedRadioButtonId
-        radioButton = findViewById(radioId)
-        Log.d(TAG, radioButton.text.toString())
+        Log.d(TAG, "hi")
 
     }
 }
