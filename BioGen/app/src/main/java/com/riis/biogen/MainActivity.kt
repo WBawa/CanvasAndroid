39a60a70ea74ebc596f9d581a0af1d2d86c47abf
Wahid Bawa <wahidbawa10@gmail.com>
2021-05-14 11:57:01 -0400

    intent stuff working

--- BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
+++ BioGen/app/src/main/java/com/riis/biogen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.riis.biogen
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,7 +12,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var radioButton: RadioButton
     private lateinit var createButton: Button
     private lateinit var spinner: Spinner
+
+    private lateinit var firstName: String
+    private lateinit var lastName: String
+    private lateinit var school: String
+    private lateinit var yearOfGraduation: String
+//     private lateinit var selectedDegree: String
     lateinit var selectedMajor: String
+    private lateinit var favouriteActivities: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +38,24 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         createButton.setOnClickListener {
 
+            firstName = (findViewById<View>(R.id.first_name) as TextView).text.toString()
+            lastName = (findViewById<View>(R.id.last_name) as TextView).text.toString()
+            school = (findViewById<View>(R.id.school) as TextView).text.toString()
+            yearOfGraduation = (findViewById<View>(R.id.year_of_graduation) as TextView).text.toString()
+//            selectedDegree = (findViewById<View>(R.id.) as TextView).text.toString()
+            favouriteActivities = (findViewById<View>(R.id.favourite_activities) as TextView).text.toString()
+
+
+            val intent = Intent(this, DisplayActivity::class.java).apply {
+               putExtra("first_name", firstName)
+                putExtra("last_name", lastName)
+                putExtra("school", school)
+                putExtra("year_of_graduation", yearOfGraduation)
+//                putExtra("selected_degree", selectedDegree)
+                putExtra("selected_major", selectedMajor)
+                putExtra("favourite_activities", favouriteActivities)
+            }
+            startActivity(intent)
         }
 
     }
