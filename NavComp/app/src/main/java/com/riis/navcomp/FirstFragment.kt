5348db6f6c9c5f907b96e427ae0057362510cfc2
Wahid Bawa <wahidbawa10@gmail.com>
2021-05-28 16:05:42 -0400

    something

---
+++ NavComp/app/src/main/java/com/riis/navcomp/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.riis.navcomp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+    private lateinit var button: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener{
+            val directions = FirstFragmentDirections.actionFirstToSecond(arg1=1234, arg2="abcd")
+            findNavController().navigate(directions)
+        }
+    }
+}
+
+}
