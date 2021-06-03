52fa60d047007552ae42dd0e57079672f04ba63d
Wahid Bawa <wbawa@riis.com>
2021-06-03 16:00:51 -0400

    there is a small bug with the year when you change the time

--- CriminalIntent/app/src/main/java/com/riis/criminalintent2/TimePickerFragment.kt
+++ CriminalIntent/app/src/main/java/com/riis/criminalintent2/TimePickerFragment.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.app.TimePickerDialog
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.TimePicker
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
@@ -11,13 +12,17 @@ import java.util.*
 
 private const val ARG_TIME = "time"
 private const val ARG_DATE = "date"
+private const val TAG = "TimePickerFragment"
 
 class TimePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val tmp = arguments?.getSerializable(ARG_DATE) as Date
         val timeListener = TimePickerDialog.OnTimeSetListener { _: TimePicker, hourOfDay: Int, minute: Int ->
-            val resultTime: Date = GregorianCalendar(0, 0, 0, hourOfDay, minute).time
 //            val resultTime: Date = GregorianCalendar(time.year, time.month, time.day, hourOfDay, minute).time
+
+            val resultTime = GregorianCalendar(tmp.year, tmp.month, tmp.day, hourOfDay, minute).time
+            Log.d(TAG, "${tmp.year}")
 
             targetFragment?.let { fragment ->
                 (fragment as DatePickerFragment.Callbacks).onDateSelected(resultTime)
